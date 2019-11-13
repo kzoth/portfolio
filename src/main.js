b19a0de026fd82c5cd808f45059e1b706912a5b5
Yegor Kazurov <kazurovegor@gmail.com>
2019-11-13 16:23:48 +0300

    week_3 dist subtree commit

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
-}
-
-import "./scripts/bg-parallax";
-import "./scripts/skill-scales";
-import "./scripts/works";
-import "./scripts/comments";
